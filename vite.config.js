bc32dfa075b2bd555a749837b3286130328a840c
João Santos <dev.joaopedrovieira@gmail.com>
2025-04-11 12:18:37 +0100

    deploy process

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,25 @@
+// vite.config.js
 import path from "path";
 import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
-import mdx from '@mdx-js/rollup'; 
+import mdx from '@mdx-js/rollup';
 import remarkFrontmatter from 'remark-frontmatter';
-import remarkMdxFrontmatter from 'remark-mdx-frontmatter'; 
+import remarkMdxFrontmatter from 'remark-mdx-frontmatter';
 import tailwindcss from '@tailwindcss/vite';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const repoName = 'decaps'; 
+
 export default defineConfig({
+  base: `/${repoName}/`,
   plugins: [
     mdx({
-      remarkPlugins: [
-        remarkFrontmatter, 
-        remarkMdxFrontmatter 
-      ],
-
+      remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
     }),
     react(),
-
     tailwindcss(),
   ],
   resolve: {
